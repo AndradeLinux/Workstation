be38ceb145a11e6996d3e497b53b8415de786031
Gabriel Andrade <77032944+AndradeLinux@users.noreply.github.com>
2022-08-16 11:16:41 -0300

    Update workstation.sh

--- workstation.sh
+++ workstation.sh
@@ -6,27 +6,35 @@ sudo rm /var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock ;
 
 ## Atualizando o repositório ##
 
-sudo apt update && 
+sudo apt update &&
+
+cd &&
 
 ## Instalando pacotes e programas do repositório deb do Ubuntu ##
 
-sudo apt install python3 python-pip git vim kdenlive gitkraken inkscape gnome-tweaks build-essential libssl-dev flatpak gnome-software-plugin-flatpak -y &&
+sudo apt install python3 python-pip git vim gnome-tweaks build-essential libssl-dev flatpak gnome-software-plugin-flatpak gnome-software -y &&
 
 ## Instalando repositótio Flatpak ##
 
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y &&
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &&
 
 ## Softwares que precisam de download externo ##
 
 cd ~/Downloads/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i *.deb &&
 
-## Instalando Pycharm ##
+## Instalando apps flatpak ##
+
+cd &&
 
 flatpak install flathub com.jetbrains.PyCharm-Community -y &&
 
-## GIMP ##
-
 flatpak install flathub org.gimp.GIMP -y &&
+
+flatpak install flathub com.axosoft.GitKraken -y &&
+
+flatpak install flathub com.stremio.Stremio -y &&
+
+flatpak install flathub com.notepadqq.Notepadqq -y &&
 
 ##Instalando driver wi-fi ##
 
@@ -34,15 +42,11 @@ sudo apt update &&
 sudo apt install dkms -y &&
 git clone -b v5.6.4.2 https://github.com/aircrack-ng/rtl8812au.git &&
 cd rtl8812au &&
-sudo make dkms_install -y &&
+sudo make dkms_install &&
 
 ## Instalando bluetooth ##
 
 sudo apt install bluetooth -y &&
-
-## Instalando Gnome-software -y ##
-
-sudo apt install gnome-software -y &&
 
 ## Atualização do sistema ##
 
@@ -54,4 +58,5 @@ echo "Finalizado"
 
 ## Reboot ##
 
-reboot &&
+reboot
+
